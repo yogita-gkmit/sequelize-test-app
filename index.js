199b3891a3ac8cb22b81b1e0cfaeb5d22200d3de
yogita-gkmit <yogita@gkmit.co>
2024-10-16 22:15:56 +0530

    docs: basic setup of the project

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+// app.use('/api', require(''));
+
+app.listen(PORT,()=>{
+	console.log(`server running on port http://localhost:${PORT}`);
+})
+
