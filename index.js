993095640f6bfe8eae55f3f308a600ffeeb0a66b
yogita-gkmit <yogita@gkmit.co>
2024-10-17 11:45:24 +0530

    feat: setup of eslint and prettier

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 // app.use('/api', require(''));
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
 	console.log(`server running on port http://localhost:${PORT}`);
-})
+});
 
+const a = 2;
