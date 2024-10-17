9029925a523affb6d228709dac3c3dd4850b5f1a
yogita-gkmit <yogita@gkmit.co>
2024-10-17 15:43:52 +0530

    feat: setup sequelize database

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize('testdb', 'pgsql', '12345678', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+
+const testDbConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+module.exports = { sq: sequelize, testDbConnection };
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-
+const { testDbConnection } = require('./config/database.js');
 const dotenv = require("dotenv");
 dotenv.config();
 
+testDbConnection();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,8 +12,22 @@ app.use(express.json());
 
 // app.use('/api', require(''));
 
+
+// const { Client } = require('pg')
+// const client = new Client({
+//   user: 'sgpostgres',
+//   host: 'SG-PostgreNoSSL-14-pgsql-master.devservers.scalegrid.io',
+//   database: 'postgres',
+//   password: 'password',
+//   port: 5432,
+// })
+// client.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+
+// });
+
 app.listen(PORT, () => {
 	console.log(`server running on port http://localhost:${PORT}`);
 });
 
-const a = 2;
